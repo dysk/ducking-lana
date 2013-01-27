30d226616ece01161cabcc85a485bc9051ac42e7
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-01-27 09:51:07 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ksiegarnia::Application.config.secret_token = '7a91cbeaa09e32af7c3b3f099caa2a1c06265730817d2a0612da7d5ae7958b0e4a8c511b6d062b65bbcf91387b2a464d37285f9dcc75d9a6e3fe69abf77023e4'
