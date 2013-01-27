9b52d79d8bfb81ee2f5e20c555de48315192ff62
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-01-27 14:55:09 +0100

    unit tests for product model

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "vat_price for product with price" do
+    p = Product.new
+    p.price = 100
+    assert_instance_of Float, p.vat_price
+    assert_equal 123, p.vat_price
+  end
+
+  test "vat_price for product without price" do
+    p = Product.new
+    assert_instance_of Float, p.vat_price
+    assert_equal 0, p.vat_price
+  end
+
+  test "product attributes cannot be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+  end
 end
