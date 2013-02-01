1dd4261c043ec79dc499ca3ac1ed13fd79cd19ed
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-02-01 14:00:40 +0100

    new schema definition, fixtures and test for store controller

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -1,9 +1,14 @@
+# encoding: UTF-8
+
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
+ test "should get index" do
+      get :index
+      assert_response :success
+      assert_select '#columns #side a', minimum: 4
+      assert_select '#main .entry', 3
+      assert_select 'h3', 'Mopsy'
+      assert_select '.price', "10.12"
   end
-
 end
