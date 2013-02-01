48db9ab82661b058c690d7b47fc258c9a2b4ad3a
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-02-01 13:56:17 +0100

    added current card method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-end
+
+    protect_from_forgery
+
+    # Dajmy dostęp w widoku
+    helper_method :current_cart
+
+    private
+
+      def current_cart
+        Cart.find(session[:cart_id])
+      rescue ActiveRecord::RecordNotFound
+        cart = Cart.create
+        session[:cart_id] = cart.id
+        cart
+      end
+  end
