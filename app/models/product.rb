57bfba2d8f8cb98d6f40061e0ad433314b9626a9
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-02-01 13:57:55 +0100

    relation to line_items and callback to check if line items refenced

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,9 @@ class Product < ActiveRecord::Base
         message: 'must be a URL for GIF, JPG or PNG image.'
       }
 
+  has_many :line_items
+
+  before_destroy :check_if_referenced_by_line_item?
 
   def vat_price
     if self.price.nil?
@@ -18,4 +21,15 @@ class Product < ActiveRecord::Base
       return self.price*1.23
     end
   end
+
+  private
+
+    def check_if_referenced_by_line_item?
+      if self.line_items.empty?
+        return true
+      else
+        self.errors.add(:base, 'Podlaczone pozycje koszyka')
+        return false
+      end
+    end
 end
