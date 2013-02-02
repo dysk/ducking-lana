129952bfb57bef292ed539be8c955399453b21b7
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-02-02 12:31:21 +0100

    mailer

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+
+class OrderNotifier < ActionMailer::Base
+  default from: "Księgarnia Ruby for Mops <biuro@rubyformops.pl>"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: @order.email
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,9 @@ class Order < ActiveRecord::Base
 
   has_many :line_items, dependent: :destroy
 
+  validates :email, :presence => true, :format => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
+
   PAYMENT_TYPES = [ "Przelew", "Płatność gotówką przy odbiorze" ]
 
   def add_line_items_from_cart(cart)
